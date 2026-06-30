f2e3ca4573e8a793ab7335867b5008030a43427d
dzgif <dzgif@users.noreply.github.com>
2026-06-30 18:27:16 +0700

    Guard kernel_compat.c session_keyring with CONFIG_KEYS

--- drivers/kernelsu/kernel_compat.c
+++ drivers/kernelsu/kernel_compat.c
@@ -10,8 +10,9 @@
 #include "klog.h" // IWYU pragma: keep
 #include "kernel_compat.h"
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 10, 0) ||                           \
-	defined(CONFIG_IS_HW_HISI) || defined(CONFIG_KSU_ALLOWLIST_WORKAROUND)
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(4, 10, 0) ||                           \
+	defined(CONFIG_IS_HW_HISI) || defined(CONFIG_KSU_ALLOWLIST_WORKAROUND)) \
+	&& defined(CONFIG_KEYS)
 #include <linux/key.h>
 #include <linux/errno.h>
 #include <linux/cred.h>
@@ -41,8 +42,9 @@ static int install_session_keyring(struct key *keyring)
 
 struct file *ksu_filp_open_compat(const char *filename, int flags, umode_t mode)
 {
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 10, 0) ||                           \
-	defined(CONFIG_IS_HW_HISI) || defined(CONFIG_KSU_ALLOWLIST_WORKAROUND)
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(4, 10, 0) ||                           \
+	defined(CONFIG_IS_HW_HISI) || defined(CONFIG_KSU_ALLOWLIST_WORKAROUND)) \
+	&& defined(CONFIG_KEYS)
 	if (init_session_keyring != NULL && !current_cred()->session_keyring &&
 	    (current->flags & PF_WQ_WORKER)) {
 		pr_info("installing init session keyring for older kernel\n");
